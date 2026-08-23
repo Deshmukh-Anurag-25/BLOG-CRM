8b48c30b4e0629b2ad3a362a04274348c9c38d05
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-23 20:40:06 +0530

    added the controllers to create, fetch all and fetch by id for users

--- backend/src/main/java/com/blogsphere/blogsphere/controller/UserController.java
+++ backend/src/main/java/com/blogsphere/blogsphere/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.blogsphere.blogsphere.controller;
 
+import com.blogsphere.blogsphere.dto.UserRequest;
+import com.blogsphere.blogsphere.model.User;
+import com.blogsphere.blogsphere.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody UserRequest request){
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
 }
