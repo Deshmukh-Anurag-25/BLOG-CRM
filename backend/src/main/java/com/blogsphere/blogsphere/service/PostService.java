22a20c688fbce10fc747e9d33dd63d11029d0408
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-23 14:04:51 +0530

    created login for creating posts

--- backend/src/main/java/com/blogsphere/blogsphere/service/PostService.java
+++ backend/src/main/java/com/blogsphere/blogsphere/service/PostService.java
@@ -1,4 +1,33 @@
 package com.blogsphere.blogsphere.service;
 
-public class PostServices {
+import com.blogsphere.blogsphere.dto.PostRequest;
+import com.blogsphere.blogsphere.model.Post;
+import com.blogsphere.blogsphere.model.User;
+import com.blogsphere.blogsphere.repository.PostRepository;
+import com.blogsphere.blogsphere.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public PostService(PostRepository postRepository, UserRepository userRepository){
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Post createPost(PostRequest request){
+        Post post = new Post();
+        post.setTitle(request.getTitle());
+        post.setSlug(request.getSlug());
+        post.setContent(request.getContent());
+        post.setExcerpt(request.getExcerpt());
+
+        User author = userRepository.findById(1L).orElseThrow();
+        post.setAuthor(author);
+
+        return postRepository.save(post);
+    }
 }
