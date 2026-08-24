bfd72494fdfb74e5c16b3d49156a1db06eec934f
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-24 23:21:47 +0530

    Controllers for bookmarks.

--- backend/src/main/java/com/blogsphere/blogsphere/controller/BookmarkController.java
+++ backend/src/main/java/com/blogsphere/blogsphere/controller/BookmarkController.java
@@ -1,4 +1,31 @@
 package com.blogsphere.blogsphere.controller;
 
+import com.blogsphere.blogsphere.model.Bookmark;
+import com.blogsphere.blogsphere.service.BookmarkService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/bookmarks")
 public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    public BookmarkController(BookmarkService bookmarkService) {
+        this.bookmarkService = bookmarkService;
+    }
+
+    @PostMapping("/post/{postId}")
+    public Bookmark bookmarkPost(@PathVariable Long postId){
+        return bookmarkService.bookmarkPost(postId);
+    }
+
+    @DeleteMapping("/post/{postId}")
+    public void removeBookmark(@PathVariable Long postId){
+        bookmarkService.removeBookmark(postId);
+    }
+
+    @GetMapping("/post/{postId}/count")
+    public long getBookmarkCount(@PathVariable Long postId){
+        return bookmarkService.getBookmarkCount(postId);
+    }
 }
