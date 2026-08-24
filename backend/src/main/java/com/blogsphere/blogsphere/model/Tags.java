2923df9bdb3d1e4d778f3a98071a2a1e15b917d6
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-24 15:42:56 +0530

    entity for a set of tags for a blog.

--- backend/src/main/java/com/blogsphere/blogsphere/model/Tags.java
+++ backend/src/main/java/com/blogsphere/blogsphere/model/Tags.java
@@ -1,4 +1,22 @@
 package com.blogsphere.blogsphere.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "tags")
 public class Tags {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
 }
