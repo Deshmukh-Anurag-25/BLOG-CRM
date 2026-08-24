d979f35dfd1637a6d6bbf6f5212846e1d945376f
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-24 15:43:49 +0530

    repository for category

--- backend/src/main/java/com/blogsphere/blogsphere/repository/CategoryRepository.java
+++ backend/src/main/java/com/blogsphere/blogsphere/repository/CategoryRepository.java
@@ -1,4 +1,7 @@
 package com.blogsphere.blogsphere.repository;
 
-public interface CategoryRepository {
+import com.blogsphere.blogsphere.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
