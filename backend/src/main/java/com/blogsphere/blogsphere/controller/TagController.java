748001dba31bf53f7627546e31eed13ee4d4a91c
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-24 15:46:34 +0530

    controller for tags for a blog.

--- backend/src/main/java/com/blogsphere/blogsphere/controller/TagController.java
+++ backend/src/main/java/com/blogsphere/blogsphere/controller/TagController.java
@@ -1,4 +1,34 @@
 package com.blogsphere.blogsphere.controller;
 
+import com.blogsphere.blogsphere.dto.TagRequest;
+import com.blogsphere.blogsphere.model.Tags;
+import com.blogsphere.blogsphere.service.TagService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tags")
 public class TagController {
+
+    private final TagService tagService;
+
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @PostMapping
+    public Tags createTag(@RequestBody TagRequest request){
+        return tagService.createTag(request);
+    }
+
+    @GetMapping
+    public List<Tags> getAllTags(){
+        return tagService.getAllTags();
+    }
+
+    @GetMapping("/{id}")
+    public Tags getTagById(@PathVariable Long id){
+        return tagService.getTagById(id);
+    }
 }
