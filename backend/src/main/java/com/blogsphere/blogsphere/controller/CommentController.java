1ada8223a6f42053fd57704e7c94476a966c159a
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-24 23:23:27 +0530

    Controllers for comments.

--- backend/src/main/java/com/blogsphere/blogsphere/controller/CommentController.java
+++ backend/src/main/java/com/blogsphere/blogsphere/controller/CommentController.java
@@ -1,4 +1,29 @@
 package com.blogsphere.blogsphere.controller;
 
+import com.blogsphere.blogsphere.dto.CommentRequest;
+import com.blogsphere.blogsphere.model.Comments;
+import com.blogsphere.blogsphere.service.CommentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public Comments createComment(@RequestBody CommentRequest request){
+        return commentService.createComment(request);
+    }
+
+    @GetMapping("/post/{postId}")
+    public List<Comments> getCommentsByPostId(@PathVariable Long postId){
+        return commentService.getCommentsByPostId(postId);
+    }
 }
