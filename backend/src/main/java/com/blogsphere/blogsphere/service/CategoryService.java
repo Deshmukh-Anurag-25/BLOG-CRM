28e6a063498cb31bfc06e8532dfac8a2fa2927cb
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-24 15:45:09 +0530

    service logic for category

--- backend/src/main/java/com/blogsphere/blogsphere/service/CategoryService.java
+++ backend/src/main/java/com/blogsphere/blogsphere/service/CategoryService.java
@@ -1,4 +1,34 @@
 package com.blogsphere.blogsphere.service;
 
+import com.blogsphere.blogsphere.dto.CategoryRequest;
+import com.blogsphere.blogsphere.model.Category;
+import com.blogsphere.blogsphere.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category createCategory(CategoryRequest request){
+        Category category = new Category();
+        category.setName(request.getName());
+        category.setSlug(request.getSlug());
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id){
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found" + id));
+    }
 }
