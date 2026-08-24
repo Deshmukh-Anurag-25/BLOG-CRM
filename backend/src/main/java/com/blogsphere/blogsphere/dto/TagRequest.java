f6a043a8d4ba2a7a11ef4e5b99c1bebac1760435
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-24 15:44:46 +0530

    dto for tag requests

--- backend/src/main/java/com/blogsphere/blogsphere/dto/TagRequest.java
+++ backend/src/main/java/com/blogsphere/blogsphere/dto/TagRequest.java
@@ -1,4 +1,11 @@
 package com.blogsphere.blogsphere.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TagRequest {
+    private String name;
+    private String slug;
 }
