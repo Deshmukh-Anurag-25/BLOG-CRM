f5f40bafa0e8d840342b5f366b61f7fec9a889cf
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-24 15:46:50 +0530

    controller for category of a blog.

--- backend/src/main/java/com/blogsphere/blogsphere/controller/CategoryController.java
+++ backend/src/main/java/com/blogsphere/blogsphere/controller/CategoryController.java
@@ -1,4 +1,34 @@
 package com.blogsphere.blogsphere.controller;
 
+import com.blogsphere.blogsphere.dto.CategoryRequest;
+import com.blogsphere.blogsphere.model.Category;
+import com.blogsphere.blogsphere.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody CategoryRequest request){
+        return categoryService.createCategory(request);
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id){
+        return categoryService.getCategoryById(id);
+    }
 }
