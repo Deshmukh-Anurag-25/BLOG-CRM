44dcae9254bb901e355ed9c8ecad09ae481ff580
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-25 14:47:02 +0530

    Controllers for likes on a post.

--- backend/src/main/java/com/blogsphere/blogsphere/controller/LikeController.java
+++ backend/src/main/java/com/blogsphere/blogsphere/controller/LikeController.java
@@ -1,4 +1,31 @@
 package com.blogsphere.blogsphere.controller;
 
+import com.blogsphere.blogsphere.model.Like;
+import com.blogsphere.blogsphere.service.LikeService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/likes")
 public class LikeController {
+
+    private final LikeService likeService;
+
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+    @PostMapping("/post/{postId}")
+    public Like likePost(@PathVariable Long postId){
+        return likeService.likePost(postId);
+    }
+
+    @DeleteMapping("/post/{postId}")
+    public void unlikePost(@PathVariable Long postId){
+        likeService.unlikePost(postId);
+    }
+
+    @GetMapping("/post/{postId}/count")
+    public long getLikesCount(@PathVariable Long postId){
+        return likeService.getLikeCount(postId);
+    }
 }
