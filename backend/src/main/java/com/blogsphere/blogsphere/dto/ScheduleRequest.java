afc48b9389dace1f8bbc57ecee33355b4ab14b56
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-26 19:38:48 +0530

    dto for schedule requests.

--- backend/src/main/java/com/blogsphere/blogsphere/dto/ScheduleRequest.java
+++ backend/src/main/java/com/blogsphere/blogsphere/dto/ScheduleRequest.java
@@ -1,4 +1,11 @@
 package com.blogsphere.blogsphere.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class ScheduleRequest {
+    private LocalDateTime scheduledAt;
 }
