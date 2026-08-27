3356d6e571a3db65c00a81992e8b867ac3a83b52
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-27 16:41:01 +0530

    added @NotBlank annotation for name and slug field in the dto.

--- backend/src/main/java/com/blogsphere/blogsphere/dto/CategoryRequest.java
+++ backend/src/main/java/com/blogsphere/blogsphere/dto/CategoryRequest.java
@@ -1,11 +1,16 @@
 package com.blogsphere.blogsphere.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CategoryRequest {
+
+    @NotBlank(message = "name is required")
     private String name;
+
+    @NotBlank(message = "slug is required")
     private String slug;
 }
