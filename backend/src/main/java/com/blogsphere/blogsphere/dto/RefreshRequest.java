caf7431fcdfdbf196d8ea6581ac10dc8bde0d47b
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-28 15:30:00 +0530

    refresh token request.

--- backend/src/main/java/com/blogsphere/blogsphere/dto/RefreshRequest.java
+++ backend/src/main/java/com/blogsphere/blogsphere/dto/RefreshRequest.java
@@ -1,4 +1,12 @@
 package com.blogsphere.blogsphere.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RefreshRequest {
+    @NotBlank
+    private String refreshToken;
 }
