04090161a283157deece404734ffcf3be15fac32
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-28 15:29:17 +0530

    request for login body.

--- backend/src/main/java/com/blogsphere/blogsphere/dto/LoginRequest.java
+++ backend/src/main/java/com/blogsphere/blogsphere/dto/LoginRequest.java
@@ -1,4 +1,16 @@
 package com.blogsphere.blogsphere.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
 }
