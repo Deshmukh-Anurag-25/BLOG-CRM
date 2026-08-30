c8599824703ca204d7de0de88d016add061906b6
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-30 14:52:31 +0530

    added findByAuthorId to the repo.

--- backend/src/main/java/com/blogsphere/blogsphere/repository/PostRepository.java
+++ backend/src/main/java/com/blogsphere/blogsphere/repository/PostRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByStatusAndScheduledAtBefore(PostStatus status, LocalDateTime time);
+    List<Post> findByAuthorId(Long authorId);
 }
