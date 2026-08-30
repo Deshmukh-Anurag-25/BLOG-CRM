b44e4ed5ef568100555a19d2eb1bc52f9418dbda
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-30 23:15:13 +0530

    added teh create, update and delete queues for comments.

--- backend/src/main/java/com/blogsphere/blogsphere/event/CommentEventListener.java
+++ backend/src/main/java/com/blogsphere/blogsphere/event/CommentEventListener.java
@@ -1,4 +1,39 @@
 package com.blogsphere.blogsphere.event;
 
+import com.blogsphere.blogsphere.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CommentEventListener {
+
+    @RabbitListener(queues = RabbitMQConfig.COMMENT_CREATED_QUEUE)
+    public void handleCommentCreated(EventEnvelope<Long> event) {
+        System.out.println("=========================================");
+        System.out.println("RECEIVED EVENT: " + event.getEventType());
+        System.out.println("Event ID: " + event.getEventId());
+        System.out.println("Comment ID: " + event.getPayload());
+        System.out.println("Timestamp: " + event.getTimestamp());
+        System.out.println("=========================================");
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.COMMENT_UPDATED_QUEUE)
+    public void handleCommentUpdated(EventEnvelope<Long> event) {
+        System.out.println("=========================================");
+        System.out.println("RECEIVED EVENT: " + event.getEventType());
+        System.out.println("Event ID: " + event.getEventId());
+        System.out.println("Comment ID: " + event.getPayload());
+        System.out.println("Timestamp: " + event.getTimestamp());
+        System.out.println("=========================================");
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.COMMENT_DELETED_QUEUE)
+    public void handleCommentDeleted(EventEnvelope<Long> event) {
+        System.out.println("=========================================");
+        System.out.println("RECEIVED EVENT: " + event.getEventType());
+        System.out.println("Event ID: " + event.getEventId());
+        System.out.println("Comment ID: " + event.getPayload());
+        System.out.println("Timestamp: " + event.getTimestamp());
+        System.out.println("=========================================");
+    }
 }
