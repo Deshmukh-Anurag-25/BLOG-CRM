6de9e21ff5e29b48260c06535521569564c284bd
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-30 23:16:42 +0530

    added the payload for following queues.

--- backend/src/main/java/com/blogsphere/blogsphere/event/FollowPayload.java
+++ backend/src/main/java/com/blogsphere/blogsphere/event/FollowPayload.java
@@ -1,4 +1,18 @@
 package com.blogsphere.blogsphere.event;
 
 public class FollowPayload {
+
+    private Long followerId;
+    private Long followingId;
+
+    public FollowPayload() {
+    }
+
+    public FollowPayload(Long followerId, Long followingId) {
+        this.followerId = followerId;
+        this.followingId = followingId;
+    }
+
+    public Long getFollowerId() { return followerId; }
+    public Long getFollowingId() { return followingId; }
 }
