11bd04a9b2584066cdaad02c5d0c0d2b05ae1356
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-09-01 22:18:47 +0530

    added two interfaces to the repository.

--- backend/src/main/java/com/blogsphere/blogsphere/repository/PendingRegistrationRepository.java
+++ backend/src/main/java/com/blogsphere/blogsphere/repository/PendingRegistrationRepository.java
@@ -1,4 +1,13 @@
 package com.blogsphere.blogsphere.repository;
 
-public interface PendingRegistrationRepository {
+import com.blogsphere.blogsphere.model.PendingRegistration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface PendingRegistrationRepository extends JpaRepository<PendingRegistration, Long> {
+    Optional<PendingRegistration> findByEmail(String email);
+    void deleteByEmail(String email);
+    void deleteByCreatedAtBefore(LocalDateTime cutoff);
 }
