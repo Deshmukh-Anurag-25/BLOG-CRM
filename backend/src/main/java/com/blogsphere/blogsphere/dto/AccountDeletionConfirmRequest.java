d87cbe891cc459bc18a5e417a64e5489323da87f
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-09-01 22:21:19 +0530

    verifying an otp for deleting an account.

--- backend/src/main/java/com/blogsphere/blogsphere/dto/AccountDeletionConfirmRequest.java
+++ backend/src/main/java/com/blogsphere/blogsphere/dto/AccountDeletionConfirmRequest.java
@@ -1,4 +1,13 @@
 package com.blogsphere.blogsphere.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AccountDeletionConfirmRequest {
+
+    @NotBlank(message = "otp is required")
+    private String otp;
 }
