9289307efd208e37c187ab9981acb615e28ae57a
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-09-01 22:20:01 +0530

    dto for message response

--- backend/src/main/java/com/blogsphere/blogsphere/dto/MessageResponse.java
+++ backend/src/main/java/com/blogsphere/blogsphere/dto/MessageResponse.java
@@ -1,4 +1,12 @@
 package com.blogsphere.blogsphere.dto;
 
 public class MessageResponse {
+
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() { return message; }
 }
